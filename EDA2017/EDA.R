3923d82ad63a061107dfd125ce81cf27bc0bba3d
SugarDodo69 <vboselli@student.42firenze.it>
2024-12-26 17:31:15 +0100

    Aggiustamento estetiche e aggiunta del fattore di scala

--- EDA2017/EDA.R
+++ EDA2017/EDA.R
@@ -4,138 +4,149 @@ library(Kendall)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Caricamento dei dati dal file Serie.txt
+# Data upload from Serie.txt
 serie <- scan("SerieR.txt")
 n <- length(serie)
 
-# Inizializzazione delle liste per x_values e y_values
+# Creation of lists for x_values and y_values
 x_values <- c()
 y_values <- c()
+m = 3 # 1 = NO SUBSAMPLE; > 1 Scale MUST BE AN INTEGER > 1
+scale <- 200 / n * m
 
-# Generazione dei valori per x_values e y_values
-for (len in 6:n) {
-  for (y in 1:(n-len+1)) {
-    centro <- len/2+y-1
+# Generation of x_values and y_values
+for (len in seq(6, n, by = m)) {
+  
+  for (y in seq(len/2, n-len/2, by = m)) {
+    centro <- y
     x_values <- c(x_values, centro)
-    y_values <- c(y_values, len)
-  }
+    y_values <- c(y_values, len)}
 }
 
 LEN_MAX <- length(x_values)
 
-# Creazione di una matrice vuota per salvare i risultati
-results <- matrix(NA, ncol = 6, nrow = LEN_MAX)
+# Creation of an empty matrix to store the results
+results <- matrix(NA, ncol = 7, nrow = LEN_MAX)
 
-# Media totale della serie
+# Global average of the serie.
 media_totale <- mean(serie)
 
-# Loop per popolare la matrice
+# Loop to populate the matrix
 for (ind in 1:LEN_MAX) {
     centro <- x_values[ind]
     len <- y_values[ind]
     start <- max(1, round(centro - (len - 1) / 2))
     end <- min(n, ceiling(centro + (len - 1) / 2))
     
-    # Estrazione della sottoserie
+    # Sub-series extraction
     sottoserie <- serie[start:end]
     
-    if (length(sottoserie) < 6) next  # Continua solo se la lunghezza è >= 6
+    if (length(sottoserie) < 6) next  # Cut off if the serie has less than 6 values
     
-    # Calcolo del valore medio della sottoserie
-    media_sottoserie <- mean(sottoserie)
-    
-    # Calcolo del t-test rispetto alla media totale
+    # Values
+    mean_subserie <- mean(sottoserie)
     t_test <- tryCatch(t.test(sottoserie, mu = media_totale)$p.value, error = function(e) NA)
-    
-    # Calcolo del trend della sottoserie (coefficiente di regressione lineare)
     trend <- tryCatch(lm(sottoserie ~ seq_along(sottoserie))$coefficients[2], error = function(e) NA)
-    
-    # Calcolo del test di Mann-Kendall sul trend della sottoserie
     mann_kendall <- tryCatch(MannKendall(sottoserie)$sl, error = function(e) NA)
     
-    # Trasformazione della matrice in un dataframe con nomi delle colonne
+    # Convert the matrix in a dataframe
     results[ind, 1] <- centro
     results[ind, 2] <- len
-    results[ind, 3] <- media_sottoserie
+    results[ind, 3] <- mean_subserie
     results[ind, 4] <- t_test
     results[ind, 5] <- trend
     results[ind, 6] <- mann_kendall
+    results[, 7] <- ifelse(results[, 4] <= 0.1, 1, 2) #MASKING
   }
 
 results <- as.data.frame(results)
 
 create_plots <- function(results) {
 
-plot1 <- ggplot(results, aes(x = V1, y = V2, color = V3)) +  # Assumendo che le colonne si chiamino V1, V2, V3
-  geom_point(size = 2) +  # Aggiungi i punti
-  scale_color_gradientn(colors = c("blue4", "blue", "cyan", "green", "yellow", "red", "red3")) +  # Gradienti di colore
-  #geom_point(data = subset(results, V4 > 0.1), aes(), size = 1, color = "white", alpha = 0.6) +
-  #geom_point(data = subset(results, V4 > 0.1), aes(), shape = 4, size = 1, color = "magenta", alpha = 0.6) +
-  labs(title = "Grafico a dispersione con colore basato su 'Valore di Z'",
-       x = "TIMEWINDOW (Time Units)",
-       y = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
-       color = "Average") +  # Etichette
-  theme_minimal()  # Tema minimalista
+### AVERAGES ###
+plot1 <- ggplot(results, aes(x = V1, y = V2, color = V3)) +
+  geom_point(size = 0.9*scale, stroke = 0.3*scale) +
+  scale_shape_manual(
+    values = c("1" = 19, "2" = 21),
+    guide = "none"
+  ) +
+  scale_color_gradientn(colors = c("blue4", "blue", "cyan", "green", "yellow", "red", "red3")) +
+  labs(title = "AVERAGES",
+       y = "TIMEWINDOW (Time Units)",
+       x = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
+       color = "Average") +
+  theme_minimal()
   plot1 <- plot1 + ggtitle("MOVING AVERAGE ANALYSIS \n Averages of all possible sub-series") +
   theme(plot.title = element_text(hjust = 0.5))
   
 CV = ceiling(max(abs(results$V5)))
 colors_neg <- colorRampPalette(c("blue4","blue","cyan"))(33)
 colors_pos <- colorRampPalette(c("yellow","red","red4"))(33)
-
-# Combina le scale colore con un contrasto netto attorno a 0
 combined_colors <- c(colors_neg,"white",colors_pos)
 
-plot2 <- ggplot(results, aes(x = V1, y = V2, color = V5)) +  # Assumendo che le colonne si chiamino V1, V2, V3
-  geom_point(size = 2) +  # Aggiungi i punti
+### TRENDS ###
+plot2 <- ggplot(results, aes(x = V1, y = V2, color = V5)) +
+  geom_point(size = 0.9*scale, stroke = 0.3*scale) +
+  scale_shape_manual(
+    values = c("1" = 19, "2" = 21),
+    guide = "none"
+  ) +
   scale_color_gradientn(
     colors = combined_colors,
     limits = c(-CV, CV)
   ) +
-  #geom_point(data = subset(results, V6 > 0.1), aes(), size = 1, color = "white", alpha = 0.6) +
-  labs(title = "Grafico a dispersione con colore basato su 'Valore di Z'",
-       x = "TIMEWINDOW (Time Units)",
-       y = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
-       color = "Average") +  # Etichette
-  theme_minimal()  # Tema minimalista
+  labs(title = "TRENDS",
+       y = "TIMEWINDOW (Time Units)",
+       x = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
+       color = "Average") +
+  theme_minimal()
   plot2 <- plot2 + ggtitle("MOVING TREND ANALYSIS \n Trends of all possible sub-series") +
   theme(plot.title = element_text(hjust = 0.5))
   
-  colors_neg_p <- colorRampPalette(c("blue4", "blue", "cyan", "green", "yellow", "red", "red3", "magenta"))(32)
-  colors_pos_p <- colorRampPalette(c("grey34","grey44","grey54", "grey64", "grey74", "grey84", "grey92", "grey98"))(33)
+colors_neg_p <- colorRampPalette(c("blue4", "blue", "cyan", "green", "yellow", "red", "red3", "magenta"))(32)
+colors_pos_p <- colorRampPalette(c("grey34","grey44","grey54", "grey64", "grey74", "grey84", "grey92", "grey98"))(33)
+combined_colors_p <- c(rev(colors_neg_p), colors_pos_p)
+
+### P-VALUES AVERAGES ###  
+plot3 <- ggplot(subset(results, V4 <= 0.2), aes(x = V1, y = V2, color = V4)) +
+  geom_point(size = 0.9*scale, stroke = 0.3*scale) +
+  scale_shape_manual(
+    values = c("1" = 19, "2" = 21),
+    guide = "none"
+  ) +
+  scale_color_gradientn(
+    colors = combined_colors_p,
+    limits = c(0, 0.2)
+  ) +
+  labs(title = "P-VALUES ON AVERAGES",
+       y = "TIMEWINDOW (Time Units)",
+       x = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
+       color = "Average") +
+  theme_minimal()
+plot3 <- plot3 + ggtitle("MOVING AVERAGE ANALYSIS \n P-values of T-tests on averages") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+### P-VALUES TREND ###  
+plot4 <- ggplot(subset(results, V6 <= 0.2), aes(x = V1, y = V2, color = V6)) +
+  geom_point(size = 0.9*scale, stroke = 0.3*scale) +
+  scale_shape_manual(
+    values = c("1" = 19, "2" = 21),
+    guide = "none"
+  ) +
+  scale_color_gradientn(
+    colors = combined_colors_p,
+    limits = c(0, 0.2)
+  ) +
+  labs(title = "P-VALUES ON TRENDS",
+       y = "TIMEWINDOW (Time Units)",
+       x = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
+       color = "Average") +
+  theme_minimal()
+plot4 <- plot4 + ggtitle("MOVING TREND ANALYSIS \n P-values of kendall's trend tests") +
+  theme(plot.title = element_text(hjust = 0.5))
   
-  # Combina le scale colore con un contrasto netto attorno a 0
-  combined_colors_p <- c(rev(colors_neg_p), colors_pos_p)
-  
-  plot3 <- ggplot(subset(results, V4 <= 0.2), aes(x = V1, y = V2, color = V4)) +  # Assumendo che le colonne si chiamino V1, V2, V3
-    geom_point(size = 2) +  # Aggiungi i punti
-    scale_color_gradientn(
-      colors = combined_colors_p,
-      limits = c(0, 0.2)
-    ) +
-    labs(title = "Grafico a dispersione con colore basato su 'Valore di Z'",
-         x = "TIMEWINDOW (Time Units)",
-         y = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
-         color = "Average") +  # Etichette
-    theme_minimal()  # Tema minimalista
-  plot3 <- plot3 + ggtitle("MOVING TREND ANALYSIS \n Trends of all possible sub-series") +
-    theme(plot.title = element_text(hjust = 0.5))
-  
-  plot4 <- ggplot(subset(results, V6 <= 0.2), aes(x = V1, y = V2, color = V6)) +  # Assumendo che le colonne si chiamino V1, V2, V3
-    geom_point(size = 2) +  # Aggiungi i punti
-    scale_color_gradientn(
-      colors = combined_colors_p,
-      limits = c(0, 0.2)
-    ) +
-    labs(title = "Grafico a dispersione con colore basato su 'Valore di Z'",
-         x = "TIMEWINDOW (Time Units)",
-         y = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
-         color = "Average") +  # Etichette
-    theme_minimal()  # Tema minimalista
-  plot4 <- plot4 + ggtitle("MOVING TREND ANALYSIS \n Trends of all possible sub-series") +
-    theme(plot.title = element_text(hjust = 0.5))
-  
-  plot <- grid.arrange(plot1, plot2, plot3, plot4, ncol = 2, nrow = 2)
+### PLOT SAVE ###
+plot <- grid.arrange(plot1, plot2, plot3, plot4, ncol = 2, nrow = 2)
   
   ggsave("temp_filename.png", plot, width = 50, height = 42, units = "cm", dpi = 600, bg = "white")
 }
--- EDA2017/EDA_CORR.R
+++ EDA2017/EDA_CORR.R
@@ -4,51 +4,53 @@ library(Kendall)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Caricamento dei dati dal file Serie.txt
-serie <- scan("SerieR.txt")
-serie2 <- scan("SerieT.txt")
+# Data upload from SerieX.txt
+serie <- scan("SerieT.txt")
+serie2 <- scan("SerieR.txt")
 n <- length(serie)
 n2 <- length(serie2)
 
-# Inizializzazione delle liste per x_values e y_values
+# Creation of lists for x_values and y_values
 x_values <- c()
 y_values <- c()
+m = 3 # 1 = NO SUBSAMPLE; > 1 Scale MUST BE AN INTEGER > 1
+scale <- 200 / n * m
 
-# Generazione dei valori per x_values e y_values
-for (len in 6:n) {
-  for (y in 1:(n-len+1)) {
-    centro <- len/2+y-1
+# Generation of x_values and y_values
+for (len in seq(6, n, by = m)) {
+  
+  for (y in seq(len/2, n-len/2, by = m)) {
+    centro <- y
     x_values <- c(x_values, centro)
-    y_values <- c(y_values, len)
-  }
+    y_values <- c(y_values, len)}
 }
 
 LEN_MAX <- length(x_values)
 
-# Creazione di una matrice vuota per salvare i risultati
-results <- matrix(NA, ncol = 6, nrow = LEN_MAX)
+# Creation of an empty matrix to store the results
+results <- matrix(NA, ncol = 7, nrow = LEN_MAX)
 
-# Loop per popolare la matrice
+# Loop to populate the matrix
 for (ind in 1:LEN_MAX) {
     centro <- x_values[ind]
     len <- y_values[ind]
     start <- max(1, round(centro - (len - 1) / 2))
     end <- min(n, ceiling(centro + (len - 1) / 2))
     
-    # Estrazione della sottoserie
+    # Sub-series extraction
     sottoserie <- serie[start:end]
     sottoserie2 <- serie2[start:end]
     
-    if (length(sottoserie) < 6) next  # Continua solo se la lunghezza è >= 6
-    # if (length(sottoserie2) < 6) next
+    if (length(sottoserie) < 6) next
 
-    # Trasformazione della matrice in un dataframe con nomi delle colonne
+    # Populate and convert the matrix in a dataframe
     results[ind, 1] <- centro
     results[ind, 2] <- len
     results[ind, 3] <- cor.test(sottoserie, sottoserie2, method = "pearson")$estimate
     results[ind, 4] <- cor.test(sottoserie, sottoserie2, method = "pearson")$p.value
     results[ind, 5] <- cor.test(sottoserie, sottoserie2, method = "kendall")$estimate
     results[ind, 6] <- cor.test(sottoserie, sottoserie2, method = "kendall")$p.value
+    results[, 7] <- ifelse(results[, 4] <= 0.1, 1, 2) #MASKING
   }
 
 results <- as.data.frame(results)
@@ -58,70 +60,84 @@ create_plots <- function(results) {
 CV = ceiling(max(abs(results$V5)))
 colors_neg <- colorRampPalette(c("blue4","blue","cyan"))(33)
 colors_pos <- colorRampPalette(c("yellow","red","red4"))(33)
-combined_colors <- c(colors_neg,colors_pos)
+combined_colors <- c(colors_neg,"white",colors_pos)
 
 colors_neg_p <- colorRampPalette(c("blue4", "blue", "cyan", "green", "yellow", "red", "red3", "magenta"))(32)
 colors_pos_p <- colorRampPalette(c("grey34","grey44","grey54", "grey64", "grey74", "grey84", "grey92", "grey98"))(33)
 combined_colors_p <- c(rev(colors_neg_p), colors_pos_p)
 
-plot1 <- ggplot(results, aes(x = V1, y = V2, color = V3)) +  # Assumendo che le colonne si chiamino V1, V2, V3
-  geom_point(size = 2) +  # Aggiungi i punti
+### PEARSON TEST ###
+plot1 <- ggplot(results, aes(x = V1, y = V2, color = V3, shape = factor(V7))) + 
+  geom_point(size = 0.9*scale, stroke = 0.3*scale) +
+  scale_shape_manual(
+    values = c("1" = 19, "2" = 21),
+    guide = "none"
+  ) +
   scale_color_gradientn(
     colors = combined_colors,
     limits = c(-1, 1)
-  ) +  # Gradienti di colore
-  #geom_point(data = subset(results, V4 > 0.1), aes(), size = 1, color = "white", alpha = 0.6) +
-  #geom_point(data = subset(results, V4 > 0.1), aes(), shape = 4, size = 1, color = "magenta", alpha = 0.6) +
-  labs(title = "Grafico a dispersione con colore basato su 'Valore di Z'",
-       x = "TIMEWINDOW (Time Units)",
-       y = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
-       color = "Pearson") +  # Etichette
-  theme_minimal()  # Tema minimalista
-plot1 <- plot1 + ggtitle("CORRELATION TESTS \n Correlations of all possible sub-series") +
+  ) +
+  labs(title = "Pearson Correlation",
+       y = "TIMEWINDOW (Time Units)",
+       x = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
+       color = "Pearson") +
+  theme_minimal() +
+  xlim(0, n) +
+  ylim(0, n)  
+plot1 <- plot1 + ggtitle("CORRELATION TESTS \n Pearson correlation coefficient of all possible sub-series") +
   theme(plot.title = element_text(hjust = 0.5))
 
-plot2 <- ggplot(subset(results, V4 <= 0.2), aes(x = V1, y = V2, color = V4)) +  # Assumendo che le colonne si chiamino V1, V2, V3
-  geom_point(size = 2) +  # Aggiungi i punti
+plot2 <- ggplot(subset(results, V4 <= 0.2), aes(x = V1, y = V2, color = V4)) +
+  geom_point(size = 0.9*scale, stroke = 0.3*scale, shape = 19) +
   scale_color_gradientn(
     colors = combined_colors_p,
     limits = c(0, 0.2)
   ) +
-  labs(title = "Grafico a dispersione con colore basato su 'Valore di Z'",
-       x = "TIMEWINDOW (Time Units)",
-       y = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
-       color = "p-Value") +  # Etichette
-  theme_minimal()  # Tema minimalista
-plot2 <- plot2 + ggtitle("CORRELATION TESTS \n Correlations of all possible sub-series") +
+  labs(title = "Pearson p-Value",
+       y = "TIMEWINDOW (Time Units)",
+       x = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
+       color = "p-Value") +
+  theme_minimal() +
+  xlim(0, n) +
+  ylim(0, n)  
+plot2 <- plot2 + ggtitle("CORRELATION TESTS \n Pearson correlation p-values for all possible sub-series") +
   theme(plot.title = element_text(hjust = 0.5))
-  
-plot3 <- ggplot(results, aes(x = V1, y = V2, color = V5)) +  # Assumendo che le colonne si chiamino V1, V2, V3
-  geom_point(size = 2) +  # Aggiungi i punti
+
+### KENDALL TEST ###
+plot3 <- ggplot(results, aes(x = V1, y = V2, color = V5, shape = factor(V7))) + 
+  geom_point(size = 0.9*scale, stroke = 0.3*scale) +
+  scale_shape_manual(
+    values = c("1" = 19, "2" = 21),
+    guide = "none"
+  ) +
   scale_color_gradientn(
     colors = combined_colors,
     limits = c(-1, 1)
-  ) +  # Gradienti di colore
-  #geom_point(data = subset(results, V4 > 0.1), aes(), size = 1, color = "white", alpha = 0.6) +
-  #geom_point(data = subset(results, V4 > 0.1), aes(), shape = 4, size = 1, color = "magenta", alpha = 0.6) +
-  labs(title = "Grafico a dispersione con colore basato su 'Valore di Z'",
-       x = "TIMEWINDOW (Time Units)",
-       y = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
-       color = "Kendall") +  # Etichette
-  theme_minimal()  # Tema minimalista
-plot3 <- plot3 + ggtitle("CORRELATION TESTS \n Correlations of all possible sub-series") +
+  ) +
+  labs(title = "Kendall Correlation",
+       y = "TIMEWINDOW (Time Units)",
+       x = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
+       color = "Kendall") +
+  theme_minimal() +
+  xlim(0, n) +
+  ylim(0, n)
+plot3 <- plot3 + ggtitle("CORRELATION TESTS \n Kendall correlation coefficient of all possible sub-series") +
   theme(plot.title = element_text(hjust = 0.5))
   
-plot4 <- ggplot(subset(results, V6 <= 0.2), aes(x = V1, y = V2, color = V6)) +  # Assumendo che le colonne si chiamino V1, V2, V3
-  geom_point(size = 2) +  # Aggiungi i punti
+plot4 <- ggplot(subset(results, V6 <= 0.2), aes(x = V1, y = V2, color = V6)) +
+  geom_point(size = 0.9*scale, stroke = 0.3*scale, shape = 19) +
   scale_color_gradientn(
     colors = combined_colors_p,
     limits = c(0, 0.2)
   ) +
-  labs(title = "Grafico a dispersione con colore basato su 'Valore di Z'",
-       x = "TIMEWINDOW (Time Units)",
-       y = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
-       color = "p-Value") +  # Etichette
-  theme_minimal()  # Tema minimalista
-plot4 <- plot4 + ggtitle("CORRELATION TESTS \n Correlations of all possible sub-series") +
+  labs(title = "Kendall p-Value",
+       y = "TIMEWINDOW (Time Units)",
+       x = "CENTRAL TIME OF THE TIME WINDOW (Time Mark)",
+       color = "p-Value") +
+  theme_minimal() +
+  xlim(0, n) +
+  ylim(0, n)  
+plot4 <- plot4 + ggtitle("CORRELATION TESTS \n Kendall correlation p-values for all possible sub-series") +
   theme(plot.title = element_text(hjust = 0.5))
   
 plot <- grid.arrange(plot1, plot2, plot3, plot4, ncol = 2, nrow = 2)
